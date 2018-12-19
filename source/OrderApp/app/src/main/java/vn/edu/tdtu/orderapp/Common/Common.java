84d9cf87599b619303415a2719baf7755476a478
windsora1998 <windsora1998@users.noreply.github.com>
2018-12-19 22:53:21 +0700

    Update code

--- source/OrderApp/app/src/main/java/vn/edu/tdtu/orderapp/Common/Common.java
+++
@@ -1,7 +0,0 @@
-package vn.edu.tdtu.orderapp.Common;
-
-import vn.edu.tdtu.orderapp.Model.User;
-
-public class Common {
-    public static User currentUser;
-}
--- source/OrderApp/app/src/main/java/vn/edu/tdtu/orderapp/Interface/ItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package vn.edu.tdtu.orderapp.Interface;
-
-import android.view.View;
-
-public interface ItemClickListener {
-    void onClick(View view, int position, boolean isLongClick);
-}
