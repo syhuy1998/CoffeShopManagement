1e14ed093f1a4c9133e8f2c83b4a57fde7027eae
tranVyTDT <40004136+tranVyTDT@users.noreply.github.com>
2018-12-22 01:40:54 +0700

    version v1.2
    update image for test result

--- training/Vy/Add2Numbers/src/add2numbers/MyBigNumber.java
+++ training/Vy/Add2Numbers/src/add2numbers/MyBigNumber.java
@@ -40,14 +40,14 @@ public class MyBigNumber {
         //use 2 loop for to find unacceptable character in 2 strings
         for (int i = 0 ; i < s1.length() ; i++)
         {
-            if (s1.charAt(i) > '9' && s1.charAt(i) < '/')
+            if (s1.charAt(i) > '9' || s1.charAt(i) < '/')
             {
                 throw new Exception("found unacceptable character in 2 strings");
             }
         }
         for (int i = 0 ; i < s2.length() ; i++)
         {
-            if (s2.charAt(i) > '9' && s2.charAt(i) < '/')
+            if (s2.charAt(i) > '9' || s2.charAt(i) < '/')
             {
                 throw new Exception("found unacceptable character in 2 strings");
             }
